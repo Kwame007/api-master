044c49305b349d53bdcc7325ac6b718a7e281292
Clement <clementnyankuadjei@gmail.com>
2025-06-17 16:09:17 +0000

    Add API client service with GET and POST methods, update app component for API interaction, and configure HTTP client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiClientService } from './services/api-client.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'api-master';
+  constructor(private apiClient:ApiClientService){
+    this.apiClient.GET().subscribe({
+      next:(data)=>{
+        console.log(data);
+      },
+      error:(error)=>{
+        console.log(error);
+      }
+    });
+  }
+
+  handleClick(){
+    this.apiClient.POST({
+      title:'foo',
+      body:'bar',
+      userId:1
+    });
+  }
 }
