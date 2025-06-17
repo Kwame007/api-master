f6fc8a25af3e7d94abb5ac785677b9d5c24ace65
Clement <clementnyankuadjei@gmail.com>
2025-06-17 16:09:17 +0000

    Add API client service with GET and POST methods, update app component for API interaction, and configure HTTP client

---
+++ src/app/services/api-client.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Post } from '../models/post.interface';
+import { catchError, Observable, throwError } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiClientService {
+
+  constructor() { }
+  private http = inject(HttpClient);
+
+  GET():Observable<Post[]>{
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').pipe(catchError(this.handleError<Post[]>));
+  }
+
+  POST(data:Post):Observable<Post>{
+    return this.http.post<Post>('https://jsonplaceholder.typicode.com/posts',data).pipe(catchError(this.handleError<Post>));
+  }
+
+  PUT(data:Post):Observable<Post>{
+    return this.http.put<Post>('https://jsonplaceholder.typicode.com/posts/1',data).pipe(catchError(this.handleError<Post>));
+  }
+
+  DELETE(id:number):Observable<Post[]>{
+    return this.http.delete<Post[]>(`https://jsonplaceholder.typicode.com/posts/${id}`).pipe(catchError(this.handleError<Post[]>));
+  }
+
+  private handleError<T>(error: any): Observable<T> {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: Error fetching posts data`;
+    }
+    return throwError(() => new Error(errorMessage));
+  }
+}
